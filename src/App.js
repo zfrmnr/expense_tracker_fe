f1f73b38f763426031ab93fb97ab3d5e96d03149
zfrmnr <zfrmnr@gmail.com>
2022-04-07 05:40:56 +0300

    list-do-things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+
+const INITIAL_STATE= [
+  {id:1, baslik:"Alisveris Yap",tamamlandı: false},
+  {id:2, baslik:"fatura ode",tamamlandı: true}
+]
+
+export default function App() {
+  const [liste ,setliste] =useState(INITIAL_STATE);
+  const [yenibaslik ,setyenibaslik] =useState("");
+  const addNew =title => {
+    setliste([...liste,{id: Date.now(),baslik:yenibaslik, tamamlandı:false}]);
+  } 
+const markCompleted =id =>{
+  setliste(liste.map(e1 => e1.id===id ? {...e1, tamamlandı: !e1.tamamlandı} :e1 ))
+}
+const clearcompleted=() =>{
+  setliste(liste.filter(item => !item.tamamlandı))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Yapılacaklar listesi</h1>
+     <div className="ekleme_formu">
+       <input value={yenibaslik} onChange={(e)=>setyenibaslik(e.target.value)} placeholder="listeye ekle" />
+       <button onClick={()=>
+        {
+          addNew(yenibaslik);
+          setyenibaslik("");
+        
+        }
+        } >Ekle</button>
+     </div>
+     <div className="liste">
+       {liste.map((item, index)=>(
+       <div 
+       key={index}
+       onClick={()=>markCompleted(item.id)}
+       className={item.tamamlandı ? "yapıldı" : " "}>{item.baslik}</div>))}
+      { /*<div>yapılacak</div>
+       <div className="yapıldı">yapıldı</div>*/}
+     </div>
+     <button onClick={()=> clearcompleted()} className="temizle">yapılacakları temizle</button>
     </div>
   );
 }
 
-export default App;
+
